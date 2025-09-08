d9253d67ab1f919d9aa9c05d028f454a699ea33c
afzallakhani <73519976+afzallakhani@users.noreply.github.com>
2025-09-08 13:18:14 +0530

    bug fix

--- utils/validateItem.js
+++ utils/validateItem.js
@@ -19,7 +19,7 @@ const itemSchema = Joi.object({
   item: Joi.object({
     itemName: Joi.string().required(),
     itemUnit: Joi.string().required(),
-    itemLife: Joi.string().required(),
+    life: Joi.string(),
     itemQty: Joi.number().required().min(0),
     itemDescription: Joi.string().required(),
     itemCategoryName: Joi.string().required(),
